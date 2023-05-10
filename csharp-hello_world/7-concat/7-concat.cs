7518142836efcb20ecd020c491cc2111175e4a95
MicoBledsoe <micomods@gmail.com>
2023-05-09 20:49:02 -0500

    [String]: concatenating 2 strings.

---
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s1 = "Welcome to ";
+        string s2 = "Holberton School!";
+        Console.WriteLine(s1 + s2);
+    }
+}
