90ee79e0f7b278f54efa4d160e7e04385b6cb97a
MicoBledsoe <micomods@gmail.com>
2023-05-09 20:10:48 -0500

    Revert back.

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -4,12 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        float num1 = 98.97f;
-        float num2 = -333.99f;
-        float num3 = 0f;
-
-        Console.WriteLine("Float: {0:F2}", num1);
-        Console.WriteLine("Float: {0:F2}", num2);
-        Console.WriteLine("Float: {0:F2}", num3);
+        float num = 3.14159f;
+        Console.WriteLine("Float: {0:F2}", num);
     }
 }
