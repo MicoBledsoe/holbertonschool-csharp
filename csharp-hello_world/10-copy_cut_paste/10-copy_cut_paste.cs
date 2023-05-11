0eeb1726d661624ca2f0c9e11e571d8206436f20
MicoBledsoe <micomods@gmail.com>
2023-05-11 14:14:29 -0500

    [Fix]: word length values

--- csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -7,7 +7,7 @@ class Program
         string word = "Holberton";
         string first3 = word.Substring(0, 3);
         string last2 = word.Substring(word.Length -2);
-        string middle = word.Substring(1, word.Length -3);
+        string middle = word.Substring(1, word.Length -2);
         Console.WriteLine("First 3 letters: {0}", first3);
         Console.WriteLine("Last 2 letters: {0}", last2);
         Console.WriteLine("Middle word: {0}", middle);
