4f83dc7bdaace55c1b591ff487b743536b0cc74a
MicoBledsoe <micomods@gmail.com>
2023-05-11 12:59:01 -0500

    [CHECKER IS BEING EXTREMELY PICKY]

--- csharp-hello_world/8-print_variables/8-print_variables.cs
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -4,8 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        string str1 = "972";
-        string str2 = "Mission Street";
-        Console.WriteLine("{0} {1}", str1, str2);
+        string str1 = "Mission";
+        string str2 = "Street";
+        int int1 = 972;
+        Console.WriteLine("{0} {1} {2}", int1, str1, str2);
     }
 }
