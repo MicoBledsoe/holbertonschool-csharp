d059bf4b596cb70cdb1bc68de3648e253d9234fb
MicoBledsoe <micomods@gmail.com>
2023-05-11 12:48:38 -0500

    [Print]: printing specified message.

---
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 972;
+        string street = "Mission Street";
+        Console.WriteLine("{0} {1}", number, street);
+    }
+}
