e0fa05f68775d01ca597f13ab7c11b86f43b40d5
MicoBledsoe <micomods@gmail.com>
2023-05-11 23:44:11 -0500

    revise

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -1,44 +1,24 @@
 using System;
 using System.Collections.Generic;
 
-class Program
+
+public class MyQueue
 {
-    // Main - entry point
-    static void Main(string[] args)
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
     {
-        // Case 1
-        Queue<string> queue1 = new Queue<string>(new[] {"C", "HTML", "Javascript", "Python", "React", "Ruby"});
-        TestQueue(queue1, "C#", "Javascript");
-
-        // Case 2
-        Queue<string> queue2 = new Queue<string>(new[] {"Cohort 0", "Cohort 1", "Cohort 2", "Cohort 3", "Cohort 4", "Cohort 5", "Cohort 6", "Cohort 7"});
-        TestQueue(queue2, "Cohort 8", "Cohort 5");
-
-        // Case 3
-        Queue<string> queue3 = new Queue<string>(new[] {"C", "HTML", "Javascript", "Python", "React", "Ruby"});
-        TestQueue(queue3, "C#", "C++");
-
-        // Case 4
-        Queue<string> queue4 = new Queue<string>();
-        TestQueue(queue4, "C#", "C++");
-    }
-
-    static void TestQueue(Queue<string> aQueue, string newItem, string search)
-    {
-        Console.WriteLine("Initial queue:");
-        foreach (string item in aQueue)
-            Console.WriteLine(item);
-
-        Console.WriteLine("------");
-
-        MyQueue.Info(aQueue, newItem, search);
-
-        Console.WriteLine("------");
-
-        Console.WriteLine("Modified queue:");
-        foreach (string item in aQueue)
-            Console.WriteLine(item);
-
-        Console.WriteLine("======");
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        bool found = aQueue.Contains(search);
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, found);
+        if (found)
+        {
+            while (aQueue.Dequeue() != search)
+                continue;
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
     }
 }
