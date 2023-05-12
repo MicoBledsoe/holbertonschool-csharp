814c34846d4a70aedfbc5de478155a46a62213ad
MicoBledsoe <micomods@gmail.com>
2023-05-11 23:42:40 -0500

    myqueue

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -1,38 +1,44 @@
 using System;
 using System.Collections.Generic;
 
-public class MyQueue
+class Program
 {
-    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    // Main - entry point
+    static void Main(string[] args)
     {
-        Console.WriteLine($"Number of items: {aQueue.Count}");
+        // Case 1
+        Queue<string> queue1 = new Queue<string>(new[] {"C", "HTML", "Javascript", "Python", "React", "Ruby"});
+        TestQueue(queue1, "C#", "Javascript");
 
-        if (aQueue.Count > 0)
-        {
-            Console.WriteLine($"First item: {aQueue.Peek()}");
-        }
-        else
-        {
-            Console.WriteLine("Queue is empty");
-        }
+        // Case 2
+        Queue<string> queue2 = new Queue<string>(new[] {"Cohort 0", "Cohort 1", "Cohort 2", "Cohort 3", "Cohort 4", "Cohort 5", "Cohort 6", "Cohort 7"});
+        TestQueue(queue2, "Cohort 8", "Cohort 5");
 
-        aQueue.Enqueue(newItem);
+        // Case 3
+        Queue<string> queue3 = new Queue<string>(new[] {"C", "HTML", "Javascript", "Python", "React", "Ruby"});
+        TestQueue(queue3, "C#", "C++");
 
-        bool containsSearch = aQueue.Contains(search);
-        Console.WriteLine($"Queue contains {search}: {containsSearch}");
+        // Case 4
+        Queue<string> queue4 = new Queue<string>();
+        TestQueue(queue4, "C#", "C++");
+    }
 
-        if (containsSearch)
-        {
-            while (aQueue.Count > 0)
-            {
-                string currentItem = aQueue.Dequeue();
-                if (currentItem == search)
-                {
-                    break;
-                }
-            }
-        }
+    static void TestQueue(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Initial queue:");
+        foreach (string item in aQueue)
+            Console.WriteLine(item);
 
-        return aQueue;
+        Console.WriteLine("------");
+
+        MyQueue.Info(aQueue, newItem, search);
+
+        Console.WriteLine("------");
+
+        Console.WriteLine("Modified queue:");
+        foreach (string item in aQueue)
+            Console.WriteLine(item);
+
+        Console.WriteLine("======");
     }
 }
