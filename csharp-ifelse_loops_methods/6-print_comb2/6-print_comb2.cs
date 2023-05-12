83135e183074ffb5c2587f303f02b347d7673bb7
MicoBledsoe <micomods@gmail.com>
2023-05-11 19:45:29 -0500

    fix value

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -4,7 +4,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        for(int dig1 = 0; dig1 <= 9; dig1++)
+        for(int dig1 = 0; dig1 <= 8; dig1++)
         {
             for(int dig2 = dig1 + 1; dig2 <= 9; dig2++)
             {
