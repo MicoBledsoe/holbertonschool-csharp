6c1b137b7bc841ce8a28cf2878de883f03e123f6
MicoBledsoe <micomods@gmail.com>
2023-05-11 23:29:18 -0500

    adds a node to the beginning of a LinkedList

--- csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
+++ csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -1,1 +1,12 @@
 using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+        myLList.AddFirst(newNode);
+        return newNode;
+    }
+}
