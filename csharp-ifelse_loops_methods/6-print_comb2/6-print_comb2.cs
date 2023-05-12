69d0a876e8533bf810381a505d10f19939075ac4
MicoBledsoe <micomods@gmail.com>
2023-05-11 19:55:23 -0500

    rewrite into differnt form since checker doesnt like the way i write stuff.

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -4,19 +4,13 @@ public class Digits
 {
     public static void Main(string[] args)
     {
-        for (int dig1 = 0; dig1 <= 8; dig1++)
+        for(int dig = 0; dig < 9; dig++)
         {
-            for (int dig2 = dig1 + 1; dig2 <= 9; dig2++)
+            for(int j = dig + 1; j < 10; j++)
             {
-                Console.Write($"{dig1}{dig2}");
-
-                if (!(dig1 == 8 && dig2 == 9))
-                {
-                    Console.Write(", ");
-                }
+                Console.Write("{0}{1}{2}", dig, j, dig == 8 ? "\n" : ", ");
             }
+            
         }
-
-        Console.WriteLine(", ");
     }
 }
