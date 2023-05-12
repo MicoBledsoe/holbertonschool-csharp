f467d59448ac9369145dac29f3fca3e9a934e920
MicoBledsoe <micomods@gmail.com>
2023-05-11 23:20:05 -0500

    stack,push,pop

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,1 +1,35 @@
 using System;
+using System.Collections.Generic;
+
+public class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+
+        if (aStack.Count > 0)
+        {
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty");
+        }
+
+        bool containsSearch = aStack.Contains(search);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, containsSearch);
+
+        if (containsSearch)
+        {
+            while (aStack.Count > 0 && aStack.Peek() != search)
+            {
+                aStack.Pop();
+            }
+            aStack.Pop();
+        }
+
+        aStack.Push(newItem);
+
+        return aStack;
+    }
+}
