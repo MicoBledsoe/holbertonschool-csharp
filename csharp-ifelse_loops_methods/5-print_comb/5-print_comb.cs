78a7f53d6d9ad09a3c2a873323c56dd849a061f9
MicoBledsoe <micomods@gmail.com>
2023-05-11 19:22:45 -0500

    count

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class CounterUp
+{
+    static void Main(string[] args)
+    {
+        for(int count = 0; count <= 100; count++)
+        {
+            Console.Write(num.ToString("00"));
+
+            if(count < 99)
+            {
+                Console.Write(", ");
+            }
+        {
+            Console.Write("\n");
+        }
+        }
+    }
+}
