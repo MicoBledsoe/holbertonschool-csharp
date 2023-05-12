3c2f1e611f6bc199a6d4f930c5e171187d015794
MicoBledsoe <micomods@gmail.com>
2023-05-11 19:48:13 -0500

    fix spacing for picky checker like always.

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -1,19 +1,17 @@
 using System;
 
-public class Program
+public class Digits
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        for(int dig1 = 0; dig1 <= 8; dig1++)
+        for (int dig1 = 0; dig1 <= 8; dig1++)
         {
-            for(int dig2 = dig1 + 1; dig2 <= 9; dig2++)
+            for (int dig2 = dig1 + 1; dig2 <= 9; dig2++)
             {
-                Console.Write($"{dig1}{dig2}");
-                if(dig1 < 8 || dig2 < 9)
-                {
-                    Console.Write(", ");
-                }
+                Console.Write($"{dig1}{dig2}, ");
             }
         }
+
+        Console.WriteLine("89");
     }
 }
