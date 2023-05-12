3a2fb06c6c52610ed1d78794e1d3a658adaccd32
MicoBledsoe <micomods@gmail.com>
2023-05-11 19:13:23 -0500

    fix

--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -1,12 +1,12 @@
 using System;
 
-public class Program
+public class HexaDec
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        for(int num = 0; num <= 99; num++)
+        for(int num = 0; num < 99; num++)
         {
-            Console.Write($"{num} = Ox{num.ToString("X")}\n");
+            Console.WriteLine("{0} = 0x{1}", num, num.ToString("x"));
         }
     }
 }
