8fefc1f0fa30416e8f28134b02faf02963bc1d3e
MicoBledsoe <micomods@gmail.com>
2023-05-11 19:52:41 -0500

    [fix]: checker doesnt like anything

--- csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -8,10 +8,15 @@ public class Digits
         {
             for (int dig2 = dig1 + 1; dig2 <= 9; dig2++)
             {
-                Console.Write($"{dig1}{dig2}, ");
+                Console.Write($"{dig1}{dig2}");
+
+                if (!(dig1 == 8 && dig2 == 9))
+                {
+                    Console.Write(", ");
+                }
             }
         }
 
-        Console.WriteLine("89");
+        Console.WriteLine(", ");
     }
 }
