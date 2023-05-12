f6900742c2146140d98c0a9189b7668331d6105b
MicoBledsoe <micomods@gmail.com>
2023-05-11 23:30:28 -0500

    returns its index position in the list

--- csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -1,1 +1,19 @@
 using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+        foreach (int item in myLList)
+        {
+            if (item == value)
+            {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+}
