b7397bf3862ff6911e4bdcf91d64a2b2fb3895f9
MicoBledsoe <micomods@gmail.com>
2023-05-11 23:21:38 -0500

    [add]:queing

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -1,1 +1,35 @@
 using System;
+using System.Collections.Generic;
+
+public class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+
+        if (aQueue.Count > 0)
+        {
+            Console.WriteLine("First item: {0}", aQueue.Peek());
+        }
+        else
+        {
+            Console.WriteLine("Queue is empty");
+        }
+
+        aQueue.Enqueue(newItem);
+
+        bool containsSearch = aQueue.Contains(search);
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, containsSearch);
+
+        if (containsSearch)
+        {
+            while (aQueue.Count > 0 && aQueue.Peek() != search)
+            {
+                aQueue.Dequeue();
+            }
+            aQueue.Dequeue();
+        }
+
+        return aQueue;
+    }
+}
