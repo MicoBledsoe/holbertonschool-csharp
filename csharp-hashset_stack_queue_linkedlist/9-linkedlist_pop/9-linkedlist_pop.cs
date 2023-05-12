1849e7760434541cdf471e9a037505b84276b173
MicoBledsoe <micomods@gmail.com>
2023-05-11 23:31:41 -0500

    deletes the head node of a LinkedList and returns that node’s data

--- csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -1,1 +1,17 @@
 using System;
+using System.Collections.Generic;
+
+public class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.First == null)
+        {
+            return 0;
+        }
+
+        int value = myLList.First.Value;
+        myLList.RemoveFirst();
+        return value;
+    }
+}
