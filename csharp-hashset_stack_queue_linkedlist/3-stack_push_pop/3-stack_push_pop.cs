03d92a5b4ce0907ce415536c3402587ec3754540
MicoBledsoe <micomods@gmail.com>
2023-05-11 23:47:05 -0500

    printing the top item, checking and printing if a specific item exists, popping items

--- csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -1,53 +1,27 @@
 using System;
 using System.Collections.Generic;
 
-class MyStack
+public class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
-        int numItems = aStack.Count;
-        Console.WriteLine("Number of items: " + numItems);
+        Console.WriteLine("Number of items: {0}", aStack.Count);
 
-        if (numItems == 0)
-        {
+        if (aStack.Count == 0)
             Console.WriteLine("Stack is empty");
-            return aStack;
-        }
-
-        string topItem = aStack.Peek();
-        Console.WriteLine("Top item: " + topItem);
-
-        bool containsSearch = false;
-        Stack<string> tempStack = new Stack<string>();
-
-        foreach (string item in aStack)
-        {
-            if (item == search)
-            {
-                containsSearch = true;
-                break;
-            }
-            tempStack.Push(item);
-        }
-
-        Console.WriteLine("Stack contains \"" + search + "\": " + containsSearch);
-
-        if (containsSearch)
-        {
-            aStack = new Stack<string>();
-
-            while (tempStack.Count > 0)
-            {
-                aStack.Push(tempStack.Pop());
-            }
-
-            aStack.Push(newItem);
-        }
         else
+            Console.WriteLine("Top item: {0}", aStack.Peek());
+
+        bool found = aStack.Contains(search);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, found);
+
+        if (found)
         {
-            aStack.Push(newItem);
+            while (aStack.Pop() != search)
+                continue;
         }
 
+        aStack.Push(newItem);
         return aStack;
     }
 }
