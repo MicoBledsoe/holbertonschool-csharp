cac4fce8b593131e8d88f03109e058b00b132a8f
MicoBledsoe <micomods@gmail.com>
2023-05-11 23:39:50 -0500

    revised code

--- csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -1,43 +1,36 @@
 using System;
 using System.Collections.Generic;
 
-class MyQueue
+public class MyQueue
 {
     public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
     {
-        int numItems = aQueue.Count;
-        Console.WriteLine("Number of items: " + numItems);
+        Console.WriteLine($"Number of items: {aQueue.Count}");
 
-        if (numItems == 0)
+        if (aQueue.Count > 0)
         {
-            Console.WriteLine("Queue is empty");
+            Console.WriteLine($"First item: {aQueue.Peek()}");
         }
         else
         {
-            string firstItem = aQueue.Peek();
-            Console.WriteLine("First item: " + firstItem);
+            Console.WriteLine("Queue is empty");
         }
 
         aQueue.Enqueue(newItem);
 
-        bool containsSearch = false;
-        Queue<string> tempQueue = new Queue<string>();
-
-        foreach (string item in aQueue)
-        {
-            if (item == search)
-            {
-                containsSearch = true;
-                break;
-            }
-            tempQueue.Enqueue(item);
-        }
-
-        Console.WriteLine("Queue contains \"" + search + "\": " + containsSearch);
+        bool containsSearch = aQueue.Contains(search);
+        Console.WriteLine($"Queue contains {search}: {containsSearch}");
 
         if (containsSearch)
         {
-            aQueue = new Queue<string>(tempQueue);
+            while (aQueue.Count > 0)
+            {
+                string currentItem = aQueue.Dequeue();
+                if (currentItem == search)
+                {
+                    break;
+                }
+            }
         }
 
         return aQueue;
