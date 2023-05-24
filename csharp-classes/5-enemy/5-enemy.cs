862195021b9671f7e18074b0f8b9d2826d3bbd38
MicoBledsoe <micomods@gmail.com>
2023-05-24 15:55:31 -0500

    task 5[]

--- csharp-classes/5-enemy/5-enemy.cs
+++ csharp-classes/5-enemy/5-enemy.cs
@@ -4,6 +4,40 @@ namespace Enemies
 {
     public class Zombie
     {
-        // Empty class definition for now
+        private int health;
+        private string name = "(No name)";
+
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int GetHealth()
+        {
+            return this.health;
+        }
+
+        public override string ToString()
+        {
+            return $"Zombie Name: {name} / Total Health: {health}";
+        }
     }
 }
