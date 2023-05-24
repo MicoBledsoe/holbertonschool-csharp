2791c3adf8c020cc6956046d7165f0f611b423b6
MicoBledsoe <micomods@gmail.com>
2023-05-24 15:47:17 -0500

    .

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -2,10 +2,19 @@ using System;
 
 namespace Enemies
 {
+    /// <summary>
+    /// Defines a Zombie.
+    /// </summary>
     public class Zombie
     {
+        /// <summary>
+        /// Represents the health of the Zombie.
+        /// </summary>
         public int health;
         
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Zombie"/> class.
+        /// </summary>
         public Zombie()
         {
             health = 0;
