890f091d8980fd4791ed662b0eda7aad2513ab6e
MicoBledsoe <micomods@gmail.com>
2023-05-24 15:54:33 -0500

    task 4[]

--- csharp-classes/4-enemy/4-enemy.cs
+++ csharp-classes/4-enemy/4-enemy.cs
@@ -1,1 +1,38 @@
 using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        private int health;
+        private string name = "(No name)";
+
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+}
