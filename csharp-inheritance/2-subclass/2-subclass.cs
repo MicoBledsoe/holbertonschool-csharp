7c0c68bf56203b7486d7e07ba07d411207c8144f
MicoBledsoe <micomods@gmail.com>
2023-05-24 17:05:19 -0500

    returns True if the object is an instance of a class that inherits from the specified class, otherwise return False

--- csharp-inheritance/2-subclass/2-subclass.cs
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -1,1 +1,12 @@
 using System;
+
+public class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.BaseType == baseType && derivedType != baseType)
+            return true;
+        else
+            return false;
+    }
+}
