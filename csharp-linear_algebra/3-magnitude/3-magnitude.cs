c08a264f05a899152cf2067b96345092fc5f1668
MicoBledsoe <micomods@gmail.com>
2023-05-31 16:56:26 -0500

    Tasks

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,1 +1,17 @@
 using System;
+
+///<summary>Vector Math class</summary>
+public static class VectorMath
+{
+    ///<summary> Vector Magnitude Method</summary>
+    public static double Magnitude(double[] vector)
+    {
+        if ( vector.Length > 3 || vector.Length < 2)
+            return -1;
+
+        double sum = 0;
+        foreach (double i in vector)
+            sum += i * i;
+        return Math.Round(Math.Sqrt(sum), 2);
+    }
+}
