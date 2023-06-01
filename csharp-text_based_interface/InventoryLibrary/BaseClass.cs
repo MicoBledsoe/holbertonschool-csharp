5959fecc063488501959e38ecd1fa30bc4e639ce
MicoBledsoe <micomods@gmail.com>
2023-06-01 09:42:37 -0500

    inventory

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InventoryLibrary
+{
+    public class BaseClass
+    {
+        public string Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+    }
+}
