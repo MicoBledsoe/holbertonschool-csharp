ad84b41e80d6e9d292e0f7a996b9910a1b9b9523
MicoBledsoe <micomods@gmail.com>
2023-06-01 11:28:44 -0500

    testing tags in Item, handling negative prices, large quantities in Inventory, saving and loading objects with JSONStorage, checking for duplicate keys, and verifying an empty file in JSONStorage.

--- csharp-text_based_interface/InventoryManagement.Tests/InventoryManagement.Tests.cs
+++ csharp-text_based_interface/InventoryManagement.Tests/InventoryManagement.Tests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using InventoryLibrary;
 
@@ -122,6 +123,72 @@ namespace InventoryManagement.Tests
             Dictionary<string, object> example = new Dictionary<string, object>();
 
             Assert.AreEqual(storage.objects.Count, example.Count);
+        }
+
+        [Test]
+        public void Item_Tags()
+        {
+            List<string> tags = new List<string> { "tag1", "tag2", "tag3" };
+            Item item = new Item("Item Name", tags: tags);
+
+            Assert.AreEqual(item.tags, tags);
+        }
+
+        [Test]
+        public void Item_PriceNegative()
+        {
+            Item item = new Item("Item Thing", "Item Description", -5.00f);
+
+            Assert.IsTrue(item.price >= 0);
+        }
+
+        [Test]
+        public void Inventory_QuantityLarge()
+        {
+            int largeQuantity = 1000000;
+            Inventory inventory = new Inventory("User ID", "Item ID", largeQuantity);
+
+            Assert.AreEqual(inventory.quantity, largeQuantity);
+        }
+
+        [Test]
+        public void JSONStorage_SaveAndLoad()
+        {
+            Item item = new Item("Item Name");
+            User user = new User("User Name");
+            Inventory inventory = new Inventory("User ID", "Item ID", 1);
+
+            storage.New(item);
+            storage.New(user);
+            storage.New(inventory);
+            storage.Save();
+
+            JSONStorage newStorage = new JSONStorage();
+            newStorage.Load();
+
+            Assert.IsNotNull(newStorage.All());
+            Assert.IsTrue(newStorage.All().ContainsKey($"{item.GetType().Name}.{item.id}"));
+            Assert.IsTrue(newStorage.All().ContainsKey($"{user.GetType().Name}.{user.id}"));
+            Assert.IsTrue(newStorage.All().ContainsKey($"{inventory.GetType().Name}.{inventory.id}"));
+        }
+
+        [Test]
+        public void JSONStorage_DuplicateKey()
+        {
+            Item item1 = new Item("Item Name");
+            Item item2 = new Item("Item Name");
+
+            storage.New(item1);
+            Assert.Throws<ArgumentException>(() => storage.New(item2));
+        }
+
+        [Test]
+        public void JSONStorage_EmptyFile()
+        {
+            JSONStorage emptyStorage = new JSONStorage();
+            emptyStorage.Load();
+
+            Assert.IsEmpty(emptyStorage.All());
         }
     }
 }
